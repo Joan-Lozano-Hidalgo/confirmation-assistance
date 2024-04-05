d7b48e3a551cc4df5f7fc9c3fd7247b9f6f1e6e3
Joan Atinohael Lozano Hidalgo <joan03lozano@gmail.com>
2024-04-04 19:33:13 -0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <title>React App</title >
+    Hola Mundo
+  </>
+)
